e4b59c146d3ec04c6b33f1f13bb1b3939a9e34f0
Unknown <mscottx88@gmail.com>
2018-09-12 16:38:30 -0700

    building coc history app

---
+++ coc-history/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+gcloud config set project oss-dev-213920
+gcloud config set compute/region us-central-1
+gcloud config set compute/zone us-central1-a
+gcloud container clusters get-credentials oss-cluster-1
+
+docker build --build-arg port=8888 --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -t gcr.io/oss-dev-213920/coc-history:1.0.0 .
+gcloud docker -- push gcr.io/oss-dev-213920/coc-history:1.0.0
+
+kubectl delete deployment --ignore-not-found=true coc-history
+kubectl apply -f deploy.yaml
